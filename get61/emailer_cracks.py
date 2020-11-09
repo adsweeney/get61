5d4af955ea5977089589a654dac76efe915f2a13
Andrew Duncan Sweeney <andrewduncansweeney@gmail.com>
2020-11-09 09:45:03 -0800

    working

---
+++ get61/emailer_cracks.py
@@ -0,0 +1,77 @@
+#!/opt/anaconda3/bin/python3
+
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+from string import Template
+
+import pandas as pd
+from scores_cracks import get_leaderboard, crack_score
+
+MY_ADDRESS = 'andrewduncansweeney@gmail.com'
+PASSWORD = 'kyzvljcflfpzobpc'
+
+def get_contacts(filename):
+    names = []
+    emails = []
+    with open(filename, mode='r', encoding='utf-8') as contacts_file:
+        for contact in contacts_file:
+            names.append(contact.split()[0])
+            emails.append(contact.split()[1])
+    return names, emails
+
+def read_template(filename):
+    with open(filename, 'r', encoding='utf-8') as template_file:
+        template_file_content = template_file.read()
+    return Template(template_file_content)
+
+def main():
+    names, emails = get_contacts('contacts.txt')
+    #message_template = read_template('message.txt')
+    userlist = {'Ben Statz' : 'Casetta Bloody Mary', 'Ryan Kelly': 'RKD', 'Gena Rieger-Olson':
+            'The L in MSLGL', 'Duncan Sweeney': 'Duncan', 'Jacob Hanke': 'jacobhanke12',
+            'Edward Mckenna': 'Dr Brad Peck', 'Thomas Gering': 'KingCrab',
+            'Andrew Wells': '3SheepsToTheWind', 'Ticho': 'Andrew Ticho', 'Brett Falkowski': 'Falko',
+            'Sam Reinertson': 'Riding Lawnmauer', 'Nate Bartz': 'moonblatz',
+            'Bret Olson': 'Bert Olson',
+            'Dan Brunner': 'DPB', 'Srdjan Gajic': 'Provie', 'James Madsen': 'James Madsen',
+            'Nate Woller': 'Woller',
+            'Person1': 'The Merrill Advantage', 'Person2': 'Best Looking Guy in the WVC',
+            
+            }
+
+    s = smtplib.SMTP(host='smtp.gmail.com', port=587)
+    s.starttls()
+    s.login(MY_ADDRESS, PASSWORD)
+
+    for name, email in zip(names, emails):
+        msg = MIMEMultipart()  # creates message.txt
+
+        #message = message_template.substitute(PERSON_NAME=name.title())
+        score_df, playercodes = get_leaderboard('https://get61.com/leaderboard?lifetime=true&sortBy=lifetime_score', userlist)
+        cracks_df = crack_score(playercodes)
+        #print(message)
+
+        msg['From'] = MY_ADDRESS
+        msg['To'] = email
+        msg['Subject'] = 'This Weeks Scores'
+
+        html = """\
+        <html>
+            <head></head>
+            <body>
+                {0}{1}
+            <body>
+        </html>
+        """.format(score_df.to_html(), cracks_df.to_html())
+        part1 = MIMEText(html, 'html')
+        msg.attach(part1)
+
+        s.send_message(msg)
+        del msg
+    s.quit()
+
+if __name__ == '__main__':
+    main()
+
--- get61/scores_cracks.py
+++ get61/scores_cracks.py
@@ -1,52 +1,78 @@
+"""[summary]
+
+Returns:
+    [type]: [description]
+"""
+import re
+from datetime import datetime
 from bs4 import BeautifulSoup
 import requests
 #import time, os
-import re
-#import urllib
+# import urllib
 #import json
 import pandas as pd
-from datetime import datetime
-import numpy as np
 
-sites = {'daily': 'https://get61.com/leaderboard', 
-        'lifetime': 'https://get61.com/leaderboard?lifetime=true&sortBy=lifetime_score',
-        'player_pages': 'https://get61.com/player_profile?player={}'}
 
-userlist = {'Ben Statz' : 'Casetta Bloody Mary', 'Ryan Kelly': 'RKD', 'Gena Rieger-Olson': 'The L in MSLGL',
-            'Duncan Sweeney': 'Duncan', 'Jacob Hanke': 'jacobhanke12', 'Edward Mckenna': 'Dr Brad Peck', 'Thomas Gering': 'KingCrab',
+
+sites = {'daily': 'https://get61.com/leaderboard',
+         'lifetime': 'https://get61.com/leaderboard?lifetime=true&sortBy=lifetime_score',
+         'player_pages': 'https://get61.com/player_profile?player={}'}
+
+userlist = {'Ben Statz' : 'Casetta Bloody Mary', 'Ryan Kelly': 'RKD', 'Gena Rieger-Olson':
+            'The L in MSLGL', 'Duncan Sweeney': 'Duncan', 'Jacob Hanke': 'jacobhanke12',
+            'Edward Mckenna': 'Dr Brad Peck', 'Thomas Gering': 'KingCrab',
             'Andrew Wells': '3SheepsToTheWind', 'Ticho': 'Andrew Ticho', 'Brett Falkowski': 'Falko',
-            'Sam Reinertson': 'Riding Lawnmauer', 'Nate Bartz': 'moonblatz', 'Bret Olson': 'Bert Olson',
-            'Dan Brunner': 'DPB', 'Srdjan Gajic': 'Provie', 'James Madsen': 'James Madsen', 'Nate Woller': 'Woller',
-            'Person1': 'The Merrill Advantage', 'Person2': 'Best Looking Guy in the WVC', 'Denman': 'Denman', 'j':'jbeam1', 'byd':'bigyellowdog'}
+            'Sam Reinertson': 'Riding Lawnmauer', 'Nate Bartz': 'moonblatz',
+            'Bret Olson': 'Bert Olson',
+            'Dan Brunner': 'DPB', 'Srdjan Gajic': 'Provie', 'James Madsen': 'James Madsen',
+            'Nate Woller': 'Woller',
+            'Person1': 'The Merrill Advantage', 'Person2': 'Best Looking Guy in the WVC',
+            'Denman': 'Denman', 'j':'jbeam1', 'byd':'bigyellowdog'
+            }
 
 
-def get_leaderboard(site, userlist):
+def get_leaderboard(site, users):
+    """Returns Lifetime Leaderboard
+
+    Args:
+        site (string): website
+        userlist (dictionary): names and usernames
+
+    Returns:
+        dataframe: leaderboard table
+    """
     page = requests.get(site).text
     soup = BeautifulSoup(page, 'html5lib')
     regser = re.compile('var names')
     datajav = soup.find(text=regser)
-    
     cats = datajav.split('"')
-    
     names = cats[1].split(',')
     score = cats[3].split(',')
     hands = cats[7].split(',')
     pickpct = cats[11].split(',')
     winpct = cats[13].split(',')
     authcodes = cats[5].split(',')
-    
-    df = pd.DataFrame(list(zip(names, score, hands, pickpct, winpct, authcodes)),
-                 columns = ['usernames', 'score', 'hands', 'pickpct', 'winpct', 'authcodes'])
-    dflim = df[df.usernames.isin(userlist.values())]
+    d_f = pd.DataFrame(list(zip(names, score, hands, pickpct, winpct, authcodes)),
+                       columns=['usernames', 'score', 'hands', 'pickpct', 'winpct', 'authcodes'])
+    dflim = d_f[d_f.usernames.isin(users.values())]
     now = datetime.now()
     dflim['timestamp'] = now.strftime("%m/%d/%Y %H:%M:%S")
-    dflim[['score', 'hands', 'pickpct', 'winpct']] = dflim[['score', 'hands', 'pickpct', 'winpct']].apply(pd.to_numeric)
+    dflim[['score', 'hands', 'pickpct', 'winpct']] = dflim[['score', 'hands', 'pickpct', 'winpct'
+                                                            ]].apply(pd.to_numeric)
 
     authdic = dict(zip(dflim['usernames'], dflim['authcodes']))
     return dflim, authdic
 
 
 def crack_score(playercodes):
+    """Lifetime crack scores
+
+    Args:
+        playercodes (dictionary): usernames with google / facebook auth identifiers
+
+    Returns:
+        dataframe: table of crack scores
+    """
     users = []
     cracks = []
     won_cracks = []
@@ -59,23 +85,20 @@ def crack_score(playercodes):
         rawsource = soup.find(text=regser)
         rawlisted = rawsource.split(';')
         rawstripped = [row.strip("'\nvar") for row in rawlisted]
-        
         stats = [line.split('=') for line in rawstripped]
-        users.append(stats[1][1].replace('"',''))
-        cracks.append(stats[2][1].replace('"',''))
-        won_cracks.append(stats[28][1].replace('"',''))
+        users.append(stats[1][1].replace('"', ''))
+        cracks.append(stats[2][1].replace('"', ''))
+        won_cracks.append(stats[28][1].replace('"', ''))
 
     crack_df = pd.DataFrame(list(zip(users, cracks, won_cracks)),
-                            columns= ['names', 'cracks_lifetime', 'cracks_won'])
-    crack_df[['cracks_lifetime', 'cracks_won']] = crack_df[['cracks_lifetime', 'cracks_won']].apply(pd.to_numeric)
+                            columns=['names', 'cracks_lifetime', 'cracks_won'])
+    crack_df[['cracks_lifetime', 'cracks_won']] = crack_df[['cracks_lifetime',
+                                                            'cracks_won']].apply(pd.to_numeric)
     crack_df['winpct_cracks'] = crack_df['cracks_won'] / crack_df['cracks_lifetime']
-    
     return crack_df
 
-score_df, playercodes = get_leaderboard(sites['lifetime'], userlist)
-crack_df = crack_score(playercodes)
+score_df, playercode = get_leaderboard(sites['lifetime'], userlist)
+crack_dff = crack_score(playercode)
 
-print(score_df.drop(['authcodes'],axis=1))
-print(crack_df.sort_values(['winpct_cracks'], ascending=True))
-
-
+print(score_df.drop(['authcodes'], axis=1))
+print(crack_dff.sort_values(['winpct_cracks'], ascending=True))
